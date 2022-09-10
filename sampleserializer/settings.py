72174f2f8995b047abd7966f8a0e9d5e7e579ba4
alfin <alfin@linkaja.id>
2022-09-11 00:47:48 +0700

    fixing code

--- sampleserializer/settings.py
+++ sampleserializer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vkw9gmxj%3q((14hdxdeha0$-q9dhvoadj0%ujwu!3h)w1jaf-'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
